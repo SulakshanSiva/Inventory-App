bdb4e3558415701074c8505baa5bbf4661d7bc71
Sulakshan <sulakshansivak@gmail.com>
2022-10-12 15:06:29 -0400

    created Info Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
+import Info from "./Info.js"
 
 function App() {
   return (
     <div className="App">
-        <p>Hello World</p>
+        <Info/>
+        <AddItem/>
+        <AddItem/>
+        <AddItem/>
     </div>
   );
+} 
+
+function AddItem(){
+    const value = "default";
+    return (
+        <form>
+            <label for="text-form">Type something: </label>
+            <input type="text" value={value} id="text-form"></input>
+        </form>
+    )
 }
 
 export default App;
