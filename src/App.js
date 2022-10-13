9f0b500ce83e5b157ec7081bd7ff66bcc7863198
Sulakshan <sulakshansivak@gmail.com>
2022-10-13 08:27:45 -0400

    created title and counter states for components

--- src/App.js
+++ src/App.js
@@ -1,38 +1,45 @@
 import './App.css';
 import Info from "./Info.js"
 import {PropTypes} from "prop-types";
+import {useState} from "react";
 
 function App() {
   return (
     <div className="App">
         <Info/>
-        <AddItem text={2} number="hello"/>
-        <AddItem text="Joe"/>
-        <AddItem/>
+        <ButtonState/>
     </div>
   );
 } 
 
-function AddItem(props){
+function ButtonState(){
+    const [title, setTitle] = useState("");
+    const [count, setCount] = useState(0);
+
+    const updateTitleClicked = () =>{
+        setTitle("We now have a title");
+    }
+
+    const updateCounterClicked = () =>{
+        setCount(count + 1);
+    }
 
     return (
-        <form>
-            <label for="text-form">Type something: </label>
-            <input type="text" value={props.text} id="text-form"></input>
-            <p>{props.number}</p>
-        </form>
-    )
+        <div>
+            <Data title={title} count={count}/>
+            <button onClick={updateTitleClicked}>Update Title</button>
+            <button onClick={updateCounterClicked}>Update Counter</button>
+        </div>
+    );
 }
 
-AddItem.defaultProps = {
-    number: 1,
-    text: "default",
-};
-
-AddItem.propTypes = {
-    number: PropTypes.number,
-    text: PropTypes.string,
-
+function Data(props){
+    return(
+        <div>
+            <p>Title: {props.title}</p>
+            <p>Count: {props.count}</p>
+        </div>
+    );
 }
 
 export default App;
--- src/Info.js
+++ src/Info.js
@@ -1,36 +1,29 @@
 import React from "react";
-import {PropTypes} from "prop-types";
 
 class Info extends React.Component {
     constructor(props){
         super(props);
-        console.log(props);
+        this.state = {
+            count : 0,
+
+        };
     }
+
+  buttonPressed(){
+    this.setState({
+        count: this.state.count + 1
+    });
+  }
 
   render() {
-    const title = this.props.title;
-    const showTitle = true;
-
-    if (showTitle) {
-      return (
+    return(
         <div>
-          <h1>{title}</h1>
-          <p>Manage your stuff.</p>
+            <p>Count: {this.state.count}</p>
+        <button onClick={() => this.buttonPressed()}>Click Me!</button>
         </div>
-      );
-    } else {
-      return <p>empty</p>;
-    }
+    );
+
   }
-}
-
-Info.defaultProps = {
-    title :"Default",
-};
-
-Info.propTypes = {
-    title: PropTypes.string,
-
 }
 
 export default Info;
