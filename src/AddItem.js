7b160398538e3fffd2a537f48288d624865320b9
Sulakshan <sulakshansivak@gmail.com>
2022-10-14 10:22:06 -0400

    created add item and display item components

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,18 +1,22 @@
 import {useState} from "react";
 
-function SearchBar(props){
+function AddItem(props){
     const [name, setName] = useState("");
     const [price, setPrice] = useState(0);
     const [type, setType] = useState("");
     const [brand, setBrand] = useState("");
 
-    const searchButtonPressed = () => {
-        props.updateSearchParams({name: name, price: price, type: type, brand: brand})
+    const addItemButtonPressed = () => {
+        props.addItem({name: name, price: price, type: type, brand: brand});
+        setName("");
+        setPrice(0);
+        setType("");
+        setBrand("");
     }
 
     return(
         <div>
-            <h2>Search For An Item</h2>
+            <h2>Add a Item</h2>
             <form>
                 <label htmlFor="name-field">Name: </label>
                 <input id="name-field" 
@@ -21,7 +25,7 @@ function SearchBar(props){
                 onChange={(e) => setName(e.target.value)}
                 />
 
-                <label htmlFor="price-field">Max Price: </label>
+                <label htmlFor="price-field">Price: </label>
                 <input id="price-field" type="number" value={price} onChange={(e) => setPrice(e.target.value)}></input>
 
                 <label htmlFor="type-field">Type: </label>
@@ -29,10 +33,10 @@ function SearchBar(props){
 
                 <label htmlFor="brand-field">Brand: </label>
                 <input id="brand-field" type="text" value={brand} onChange={(e) => setBrand(e.target.value)}></input>
-                <button type="button" onClick={searchButtonPressed}>Search</button>
+                <button type="button" onClick={addItemButtonPressed}>Add Item</button>
             </form>
         </div>
     );
 }
 
-export default SearchBar;
+export default AddItem;
